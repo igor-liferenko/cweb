208b267a0f48528a7d48ac5a6a2e6028ed309ef6
Igor Liferenko <igor.liferenko@gmail.com>
2017-06-13 10:21:59 +0700

    save output from cweave and ctangle

--- utf8/build.sh
+++ utf8/build.sh
@@ -14,10 +14,13 @@
 # git checkout .
 # git reset .
 # git clean -f
-# ./runall.sh -p /bin/ >/dev/null
-# ./runall.sh -p /usr/local/bin/ >/dev/null # OR /var/local/bin/
+# ./runall.sh -p /bin/ >runall.log
+# NOTE: we use runall2.log instead of putting the next line before second "git archive" and redirecting to runall.log, because otherwise you will get "your local changes would be overwritten by checkout" error on runall.log when you will run runall.sh for the second time
+# ./runall.sh -p /usr/local/bin/ >runall2.log # OR /var/local/bin/
 # git archive runall-/bin/-V | command tar -xf -
 # git add .
+# git reset runall2.log
+# mv runall2.log runall.log
 # git archive runall-/usr/local/bin/-V | command tar -xf -
 # git branch -D runall-/bin/-V runall-/usr/local/bin/-V
 #
