51bea7503f6d65b151d5e2576815c008c9918cc7
Igor Liferenko <igor.liferenko@gmail.com>
2018-01-31 12:43:27 +0700

    edit

--- utf8/test-cwebmac.sh
+++ utf8/test-cwebmac.sh
@@ -19,52 +19,28 @@ git rev-parse --abbrev-ref HEAD | grep -v master && exit
 git diff --exit-code HEAD || exit
 cd - >/dev/null
 cp /home/user/cweb/cwebmac.tex .
-perl -i -ne 'print unless /ensure that the contents file/' cwebmac.tex # fix nonsense #TODO:edit not cwebmac.tex, but .toc files after dvi files were generated
-#TODO: check if it makes any difference with and without this: \setbox\sbox\vbox{\unvbox\sbox} % take it out of its box
 for i in *.tex; do [ $i = cwebmac.tex ] && continue; [ $i = epsf.tex ] && continue; tex $i; done &>/dev/null
+for i in *.toc; do perl -i -0777 -pe 's/\n(?=\\catcode `\\@=12\\relax)//' $i; done # this is removed in my cwebmac
 for i in *.dvi; do dvihash $i; done >hash.all
 git add .
 git commit -m 'tex' >/dev/null
 git checkout runall-/usr/local/bin/-V &>/dev/null
 for i in *.mp; do mpost $i; done >/dev/null
-patch -F0 tcb.tex << EOF >/dev/null || exit
-@@ -82,16 +82,15 @@
- 
- % Now I bring in the program files created with cweave
- 
--\input cwebmac
-+\input cwebmal
- \let\INPUT=\input
- \def\input#1 {\def\next{#1}\ifx\next\cwebmac\else\INPUT #1\fi}
--\def\cwebmac{cwebmac}
-+\def\cwebmac{cwebmal}
- \def\inx{}
- \def\fin{}
- \def\con{\output{\setbox0=\box255
-                 \global\output{\normaloutput\page\lheader\rheader}}\eject}
- \outer\def\N#1#2#3.{% beginning of starred section
+patch tcb.tex <<EOF || exit # pdf mode is not used in my cwebmac
+@@ -94 +93,0 @@
 -  \ifacro{\toksF={}\makeoutlinetoks#3\outlinedone\outlinedone}\fi
-   \gdepth=#1\gtitle={#3}\MN{#2}%
-   \ifon\ifnum#1<\secpagedepth \vfil\eject % force page break if depth is small
-     \else\vfil\penalty-100\vfilneg\vskip\intersecskip\fi\fi
-@@ -99,11 +98,6 @@
-   \def\stripprefix##1>{}\def\gtitletoks{#3}%
-   \edef\gtitletoks{\expandafter\stripprefix\meaning\gtitletoks}%
-  % omit output to contents file
+@@ -102,5 +100,0 @@
 -  \ifpdftex\expandafter\xdef\csname curr#1\endcsname{\secno}
 -    \ifnum#1>0\countB=#1 \advance\countB by-1
 -      \advancenumber{chunk\the\countB.\expnumber{curr\the\countB}}\fi\fi
 -  \ifpdf\special{pdf: outline #1 << /Title (\the\toksE) /Dest
 -    [ @thispage /FitH @ypos ] >>}\fi
-   \ifon\startsection{\bf#3.\quad}\ignorespaces}
- 
- \def\datethis{\def\startsection{\centerline{\bf Program 1}\bigskip
 EOF
 cp /usr/local/SUPER_DEBIAN/epsf.tex .
 cp /usr/local/SUPER_DEBIAN/lhplain.ini .
 perl -i -pe 's/(?=\\dump)/\\def\\time{5}\n/' lhplain.ini
 tex -ini -jobname tex lhplain.ini >/dev/null
-for i in *.tex; do [ $i = cwebmac.tex ] && continue; [ $i = epsf.tex ] && continue; tex $i; done &>/dev/null
+for i in *.tex; do [ $i = epsf.tex ] && continue; tex $i; done &>/dev/null
 for i in *.dvi; do dvihash $i; done >hash.all
 git add .
 git commit -m 'tex' >/dev/null
