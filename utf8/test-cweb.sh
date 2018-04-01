2651cc292b5dd6acd19d28e931c7b9a465200b2b
Igor Liferenko <igor.liferenko@gmail.com>
2018-04-01 08:17:09 +0700

    edit

--- utf8/test-cweb.sh
+++ utf8/test-cweb.sh
@@ -8,17 +8,14 @@ git checkout master &>/dev/null
 git branch -D runall-/bin/-V runall-/usr/local/bin/-V &>/dev/null
 sed -i '0,/^@ /s//@* Intro. /' reflect.w # see tex/question2.tex
 ./runall.sh -p /bin/ >runall.log 2>/dev/null
-git checkout reflect.w
 git checkout runall-/bin/-V &>/dev/null
 git add runall.log
 git commit -m 'runall' >/dev/null
 git checkout master &>/dev/null
-sed -i '0,/^@ /s//@* Intro. /' reflect.w
 ./runall.sh -p /usr/local/bin/ >runall.log 2>/dev/null
-git checkout reflect.w
 git checkout runall-/usr/local/bin/-V &>/dev/null
 git add runall.log
 git commit -m 'runall' >/dev/null
-git commit -m 'cwebmac' . >/dev/null
 git checkout master &>/dev/null
 git diff runall-/bin/-V runall-/usr/local/bin/-V
+git checkout reflect.w
