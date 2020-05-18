ab04a43a32aaea4ca38cf27834f8c652afe8cb2d
Igor Liferenko <igor.liferenko@gmail.com>
2020-05-18 13:05:16 +0700

    use '-q' in 'git checkout' and add '3.64' to '...-V' branch names

--- utf8/test-cweb.sh
+++ utf8/test-cweb.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 cd /home/user/cwebtest/
-git checkout .
+git checkout -q .
 git reset >/dev/null
-git checkout .
+git checkout -q .
 git clean -f >/dev/null
 git checkout master &>/dev/null
-git branch -D runall-/bin/-V runall-/usr/local/bin/-V &>/dev/null
+git branch -D runall-/bin/-V3.64 runall-/usr/local/bin/-V3.64 &>/dev/null
 sed -i '0,/^@ /s//@* Intro. /' reflect.w # see tex/question2.tex
 ./runall.sh -p /bin/ >runall.log 2>/dev/null
-git checkout runall-/bin/-V &>/dev/null
+git checkout runall-/bin/-V3.64 &>/dev/null
 git add runall.log
 git commit -m 'runall' >/dev/null
 git checkout master &>/dev/null
 ./runall.sh -p /usr/local/bin/ >runall.log 2>/dev/null
-git checkout runall-/usr/local/bin/-V &>/dev/null
+git checkout runall-/usr/local/bin/-V3.64 &>/dev/null
 git add runall.log
 git commit -m 'runall' >/dev/null
 git checkout master &>/dev/null
-git diff runall-/bin/-V runall-/usr/local/bin/-V
-git checkout reflect.w
+git diff runall-/bin/-V3.64 runall-/usr/local/bin/-V3.64
+git checkout -q reflect.w
